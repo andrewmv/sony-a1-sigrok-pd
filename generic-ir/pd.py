146f2b04fab81078cf9e8f9156816e541d08386a
Andrew Villeneuve <andrewmv@gmail.com>
2026-05-18 18:36:24 -0700

    Added generic IR decoder

---
+++ generic-ir/pd.py
@@ -0,0 +1,128 @@
+##
+## Copyright (C) 2021 Andrew Villeneuve <andrewmv@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+import sigrokdecode as srd
+
+PW_INIT = 4700
+PW_ZERO = 380
+PW_ONE = 180
+
+RC_INIT = 3
+
+STATE_FIND_INIT = 0
+STATE_READ_CMD = 2
+
+class SamplerateError(Exception):
+    pass
+
+class Decoder(srd.Decoder):
+    api_version = 3
+    id = 'generic-ir'
+    name = 'Generic IR'
+    longname = 'Another Generic IR Decoder'
+    desc = '38khz infrared'
+    license = 'gplv2+'
+    inputs = ['logic']
+    outputs = ['insignia']
+    channels = (
+        {'id': 'data', 'name': 'IR DATA', 'desc': 'IR Data'},
+    )
+    options = (
+    )
+    annotations = (
+        ('bits', 'Bits'),
+        ('command', 'Command'),
+        ('command_msb', 'MSB First'),
+        ('command_inv', 'Inverted'),
+        ('command_msb_inv', '~MSB'),
+    )
+    annotation_rows = (
+        ('bits', 'Bits', (0, )),
+        ('data_lsb', 'Data LSB', (1, )),
+        ('data_msb', 'Data MSB', (2, )),
+        ('data_ilsb', 'Data ~LSB', (3, )),
+        ('data_imsb', 'Data ~MSB', (4, )),
+    )
+
+    def __init__(self):
+        self.samplerate = None
+        self.bitcount = 0
+        self.databyte = 0
+
+    def metadata(self, key, value):
+        if key == srd.SRD_CONF_SAMPLERATE:
+            self.samplerate = value
+
+    def start(self):
+        self.out_python = self.register(srd.OUTPUT_PYTHON)
+        self.out_ann = self.register(srd.OUTPUT_ANN)
+        self.state = STATE_FIND_INIT
+        self.bitcount = 0
+
+    def reset(self):
+        self.state = STATE_FIND_INIT
+        self.bitcount = 0        
+
+    def handle_pulse(self): 
+        # Find falling edge
+        self.wait({0: 'f'})
+        edge_start = self.samplenum
+        if (self.bitcount == 0):
+            self.byte_start = self.samplenum
+
+        # Find rising edge
+        self.wait({0: 'r'})
+        edge_end  = self.samplenum
+
+        # Determine pulse width in microseconds
+        pulse_width = ((edge_end - edge_start) / self.samplerate) * 1000 * 1000
+
+        if (pulse_width > PW_INIT):
+            self.put(edge_start, edge_end, self.out_ann, [0, ["INIT", "IN"]])
+            self.bitcount = 0
+            self.databyte = 0
+            return RC_INIT
+        elif (pulse_width > PW_ZERO):
+            self.put(edge_start, edge_end, self.out_ann, [0, ["0"]])
+            self.bitcount += 1
+            return 0
+        # elif (pulse_width > PW_ONE):
+        #     self.put(edge_start, edge_end, self.out_ann, [0, ["1"]])
+        #     self.bitcount += 1
+        #     return 1
+        else: 
+            self.put(edge_start, edge_end, self.out_ann, [0, ["1"]])
+            self.bitcount += 1
+            return 1
+
+    def decode(self):
+        while True:
+            bit = self.handle_pulse()
+            if (bit == RC_INIT):
+                # Reset the state machine whenever we see an initialzation pulse
+                self.state = STATE_READ_CMD
+            elif (self.state == STATE_READ_CMD):
+                self.databyte = self.databyte | ((bit << (8 - self.bitcount)))
+                if (self.bitcount >= 8):
+                    byte_end = self.samplenum
+                    self.invbyte = self.databyte ^ 0xFF
+                    self.put(self.byte_start, byte_end, self.out_ann, [1, ["0x{:02X}".format(self.databyte), "{:02X}".format(self.databyte)]])
+                    self.put(self.byte_start, byte_end, self.out_ann, [2, ["0x{:02X}".format(self.reverse_bits(self.databyte)), "{:02X}".format(self.reverse_bits(self.databyte))]])
+                    self.put(self.byte_start, byte_end, self.out_ann, [3, ["0x{:02X}".format(self.invbyte), "{:02X}".format(self.invbyte)]])
+                    self.put(self.byte_start, byte_end, self.out_ann, [4, ["0x{:02X}".format(self.reverse_bits(self.invbyte)), "{:02X}".format(self.reverse_bits(self.invbyte))]])
+                    self.bitcount = 0
+                    self.databyte = 0
+
+    def reverse_bits(self, b: int) -> int:
+        return int(f'{b:08b}'[::-1], 2)
+
